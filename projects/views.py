d20ebf34cb20048b075cdb4f4b04a8dd94110b98
Teza Alfian <44139001+tezaalfian@users.noreply.github.com>
2023-08-27 16:13:08 +0700

    code refactoring before thesis Defence

--- projects/views.py
+++ projects/views.py
@@ -12,6 +12,7 @@ from io import BytesIO
 import base64
 import matplotlib.pyplot as plt
 from statsmodels.tsa.api import SimpleExpSmoothing
+import statsmodels.api as sm
 
 def week_number_of_month(date_value):
     return (date_value.isocalendar()[1] - date_value.replace(day=1).isocalendar()[1] + 1)
@@ -25,7 +26,7 @@ def result(request):
     # ARIMA
     df_train = df[:-8]
     df_test = df[-8:]
-    arima_params = (3, 2, 0)
+    arima_params = (4, 0, 5)
     arima_model = ARIMA(df_train, order=arima_params)
     arima_train = arima_model.fit()
     arima_test = arima_train.forecast(steps=len(df_test))
@@ -39,7 +40,7 @@ def result(request):
     arima_forecast = pd.concat([arima_test[-1:], arima_forecast])
 
     # SES
-    alpha = 0.35
+    alpha = 0.43
     ses_test = pd.DataFrame()
     for i in range(len(df_test), 0, -1):
         model = SimpleExpSmoothing(df[:-i])
@@ -78,13 +79,19 @@ def result(request):
     data_test = []
     for index, row in df_test.iterrows():
         # create object date, amount, forecast arima & ses
+        # if mape > 1 then invalid
+        mape_arima = round(mean_absolute_percentage_error([row['amount']], [arima_test.loc[index]['amount']]), 3)
+        mape_ses = round(mean_absolute_percentage_error([row['amount']], [ses_test.loc[index]['amount']]), 3)
+        if mape_arima > 1 and mape_ses > 1:
+            mape_arima = 'Invalid'
+            mape_ses = 'Invalid'
         data_test.append({
             "date": index.strftime("%b %Y") + ' Minggu ke-' + str(week_number_of_month(index)),
             "amount": row['amount'],
             "arima": arima_test.loc[index]['amount'],
             "ses": ses_test.loc[index]['amount'],
-            "mape_arima": round(mean_absolute_percentage_error([row['amount']], [arima_test.loc[index]['amount']]), 3),
-            "mape_ses": round(mean_absolute_percentage_error([row['amount']], [ses_test.loc[index]['amount']]), 3)
+            "mape_arima": mape_arima,
+            "mape_ses": mape_ses
         })
     data_forecast = []
     for index, row in arima_forecast[1:].iterrows():
@@ -93,10 +100,16 @@ def result(request):
             "arima": row['amount'],
             "ses": ses_forecast.loc[index]['amount']
         })
+    if arima_mape > 1 and ses_mape > 1:
+        arima_mape = 'Invalid'
+        ses_mape = 'Invalid'
+    else:
+        arima_mape = round(arima_mape * 100, 1)
+        ses_mape = round(ses_mape * 100, 1)
     return render(request, "result.html", {
         "graph_test": graph_test,
-        "arima_mape": round(arima_mape * 100, 2),
-        "ses_mape": round(ses_mape * 100, 2),
+        "arima_mape": arima_mape,
+        "ses_mape": ses_mape,
         'arima_forecast': arima_forecast,
         'ses_forecast': ses_forecast,
         'data_test': data_test,
